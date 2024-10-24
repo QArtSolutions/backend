4542cf5f8e504f5572748df6c29c06271a7f4e5a
Utkarsh_Agrawal <Utkarshagrawalindia@gmail.com>
2024-10-24 16:00:16 +0530

    Login connected with database

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 npm install dotenv
 npm install sequelize@latest
 npm install pg@latest
-npm install bcryptjs
+npm install bcrypt
 npm install express
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const userController = require('./infrastructure/controllers/userController');
 require('dotenv').config();
 
 const app = express();
+
+// Logging middleware to check all incoming requests
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+// Use cors middleware
+app.use(cors());
 
 // Middleware
 app.use(bodyParser.json());
