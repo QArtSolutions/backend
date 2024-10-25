271b1ad8395c96ed2677c09043a11472071f98e8
Dell <Dell@SANSKAR>
2024-10-25 10:03:45 +0530

    [NEW] added new feature

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const userController = require('./infrastructure/controllers/userController');
 require('dotenv').config();
+const cors = require('cors');
+
 
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
+app.use(cors()); // Enable CORS for all routes
+
 
 // Routes
 app.use('/api/users', userController);
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- src/infrastructure/controllers/userController.js
+++ src/infrastructure/controllers/userController.js
@@ -5,6 +5,13 @@ const router = express.Router();
 // POST /register - Register a new user
 router.post('/register', async (req, res) => {
   const { username, email, password } = req.body;
+  if (password.length < 7) {
+    return res.status(400).json({ message: 'Password must be at least 7 characters long.' });
+  }
+  const usernamePattern = /^[a-zA-Z0-9 ]+$/;
+  if (!usernamePattern.test(username)) {
+    return res.status(400).json({ message: 'Username can only contain letters, numbers, and spaces.' });
+  }
   try {
     const user = await registerUser(username, email, password);
     res.status(201).json(user);
